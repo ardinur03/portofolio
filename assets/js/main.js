126c9fdb053f7d4dbab09bdcb880ef311eab5db9
ardinur03 <ardinurinsan03@gmail.com>
2021-06-14 18:28:35 +0700

    UPDATE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile*/
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
@@ -34,45 +34,55 @@ const sr = ScrollReveal({
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.home__title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', { delay: 400 });
+sr.reveal('.about__text', { delay: 400 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', { interval: 200 });
+sr.reveal('.skills__img', { delay: 600 });
+
+// Timeline
+sr.reveal('.title-timeline', {});
+sr.reveal('.timeline', {});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.work__img', { interval: 200 });
 
 /*SCROLL CONTACT*/
 // sr.reveal('.contact__input',{interval: 200}); 
 
 
+$(function() {
+    $('[data-tugle="tooltip"]').tooltip('show')
+})
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
 
 
-new Typed('#typed',{
-    strings : ['I\'am Blogger','I\'am junior Programmer','I\'am Student','Vocational High School in Bandung'],
-    typeSpeed : 100,
-    delaySpeed : 600,
-    loop : true
+new Typed('#typed', {
+    strings: ['I\'am Blogger', 'I\'am junior Programmer', 'I\'am Student', 'Vocational High School in Bandung'],
+    typeSpeed: 100,
+    delaySpeed: 600,
+    loop: true
 });
 
 
 
-function getCv(){
+function getCv() {
     Swal.fire({
         title: 'Info !!!',
-        html:'Mohon maaf karena <b>keamanan & privasi.</b> Untuk mendownload CV saya hubungi <br>email: <b> ardinurinsan03@gmail.com</b></br> ' +
-             '<a class="btn btn-success btn-sm" href="mailto:ardinurinsan03@gmail.com">Kirim Email</a>',
+        html: 'Mohon maaf karena <b>keamanan & privasi.</b> Untuk mendownload CV saya hubungi <br>email: <b> ardinurinsan03@gmail.com</b></br> ' +
+            '<a class="btn btn-success btn-sm" href="mailto:ardinurinsan03@gmail.com">Kirim Email</a>',
         icon: 'warning',
         showCancelButton: true,
         showConfirmButton: false,
